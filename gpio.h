a4cf8df093e974584fffff64a8c9fbbdc2407389
ecastan960 <32081995+ecastan960@users.noreply.github.com>
2019-12-14 08:35:18 -0500

    Add files via upload

---
+++ gpio.h
@@ -0,0 +1,3 @@
+import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
